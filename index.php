377878eb5198e772809f683506cca45ea1711f0c
Arman Sani <armanisani@gmail.com>
2016-04-22 15:27:57 -0700

    starting to loop through every workout through a 'days' array with php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/index.css" media="screen" title="no title" charset="utf-8">
+    <title>My PHP app</title>
+  </head>
+  <body>
+    <div class="header">
+      <h1 >
+    <?php
+    // $_SESSION['post-data'] = $_POST;
+      echo "Workout Plan" ;
+      days 
+      $monday = Array(Array('Jogging', 'Chestpress', 'Incline Press', 'Decline Press', 'Situps' ), Array(1,4,"3-4", "3-4", 5),Array('5 mins', "8-12", "8-12", "8-12", 40));
+      $tuesday = Array(Array('Jogging', 'Chestpress', 'Incline Press', 'Decline Press', 'Situps' ), Array(1,4,"3-4", "3-4", 5),Array('5 mins', "8-12", "8-12", "8-12", 40));
+      // array_push($ns, $name);
+      // for ()
+    '<h4>';
+      echo "<h2>"."</h2>";
+      echo sizeOf($monday[0]);
+      $x = 0;
+      while($x <= sizeOf($monday[0])-1) {
+      echo "Workout: " . $monday[0][$x] . "<br/> Sets: " . $monday[1][$x] . "<br/> Duration: " . $monday[2][$x] .'<br/><br/>';
+      $x++;
+    }
+    '</h4>';
+
+      ?>
+    <div>
+  </body>
+</html>
